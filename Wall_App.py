e31e9facd67de694a40ff9ffdf49506e00ff6446
musv1901 <sven.mueller.3@hof-university.de>
2023-05-21 15:00:53 +0200

    changes

--- Wall_App.py
+++ Wall_App.py
@@ -1,6 +1,7 @@
 import time
 from Wall_Model import WallModel
 from Wall_View import WallView
+from confluent_kafka import Consumer
 
 if __name__ == "__main__":
     model = WallModel()
@@ -8,8 +9,17 @@ if __name__ == "__main__":
     p_list = model.get_db_entries()
     view.refresh_img_wall(p_list)
 
+    consumer = Consumer({
+        'bootstrap.servers': '192.168.70.40:9092',
+        'group.id': 'py',
+        'auto.offset.reset': 'earliest'
+    })
+    consumer.subscribe(['toUI'])
+
     while True:
-        if int(time.time() - view.start_time) > 10 and not view.refresh_stop:
+        msg = consumer.poll(1.0)
+        if msg and not view.refresh_stop:
+            print("Message consumed")
             p_list = model.get_db_entries()
             view.refresh_img_wall(p_list)
             view.start_time = time.time()
